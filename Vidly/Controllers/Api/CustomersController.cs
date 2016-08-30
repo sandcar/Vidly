a805882cd02d9822b2f820afbe502c18b523247b
coliveira <karlosoliveira@hotmail.com>
2016-08-30 15:27:49 +0100

    Implemented first web api module

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+
+            _context = new ApplicationDbContext();
+
+        }
+
+        // Get/Aoi/Customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // Get/Aoi/Customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST/Aoi/Customers/
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT/Aoi/Customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.Single(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb.Name = customer.Name;
+            customerInDb.BirthDate = customer.BirthDate;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.MemberShipTypeId = customer.MemberShipTypeId;
+
+            _context.SaveChanges();
+
+
+        }
+
+
+        // DELETE/Aoi/Customers/1
+        [HttpDelete]
+        public void dELETECustomer(int id)
+        {
+
+            var customerInDb = _context.Customers.Single(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
